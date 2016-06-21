7dcd5391321c662efb396844ee6992f178739792
amanokh <a.manokh@gmail.com>
2016-06-21 18:22:39 +0800

    Update friends_ge3t.py
    Added comments

--- friends_ge3t.py
+++ friends_ge3t.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import http.client, urllib, statistics
+import http.client, urllib
 schools={}
 sch_count={}
-token=input('token (for private users, can press Enter):')
+token=input('token:')                     #for getting a school info of private user, may skip
 user_id=input('userid:')
 print ("")
 print ("Connecting to VK...")
@@ -17,23 +17,23 @@ conn.request("POST", "/method/friends.get", params)
 response = conn.getresponse()
 print (response.status, response.reason)
 answer = response.read()
-dic=eval(answer)
+dic=eval(answer)            #transform json answer to dictionary
 
 print ("Parsing users...")
 pupils=int(dic['response']['count'])
 for i in dic['response']['items']:
     try:
-        for sc in i['schools']:
+        for sc in i['schools']:         #for people with 2 or more schools in profile
             scid=sc['id']
-            schools[scid]='%s, %s' % (sc['name'], sc['city'])
+            schools[scid]='%s, %s' % (sc['name'], sc['city'])       #adding a school info to 'schools' dictionary
             sch_count[scid]=sch_count.get(scid, 0)+1
     except KeyError:
         z=0
     except IndexError:
         z=0
-sch_count=sorted(sch_count.items(), key=lambda x: x[1])
+sch_count=sorted(sch_count.items(), key=lambda x: x[1])     #sorting schools by amount of it's pupils-friends
 k=-1
-while k!=-4:
+while k!=-4:            #top-3 results
     percent=round((sch_count[k][1]/pupils)*100, 2)
     print ('%s ---- %s' % (schools[sch_count[k][0]], percent), '%,', '%s of %s friends' % (sch_count[k][1], pupils))
     k-=1
